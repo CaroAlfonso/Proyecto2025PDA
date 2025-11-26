b26a722c62ff7c82cbd845ab68859cb2bfc9fe98
Carolina Alfonso <carolina.alfonso@live.com>
2025-11-26 15:16:13 -0300

    Corregidas rutas de imágenes en carrito

--- js/carrito.js
+++ js/carrito.js
@@ -3,7 +3,6 @@ import { eliminarDelCarrito, vaciarCarrito } from "./funcionesCarrito.js";
 import { actualizarContador } from "./ui.js";
 
 const renderizarCarrito = () => {
-    const basePath = window.location.pathname.includes("/pages/") ? "../" : "";
     
     const carrito = obtenerCarrito();
     actualizarContador(carrito);
